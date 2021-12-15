ed126951127277a68a3f2d9d92d3665043f4eb95
shindeanant <shindeanant1997@gmail.com>
2021-12-15 19:17:54 +0530

    Added:Employee Payroll Spring boot Project file added.

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayRollAppDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayRollAppDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayRollAppDevelopmentApplication.class, args);
+	}
+
+}
