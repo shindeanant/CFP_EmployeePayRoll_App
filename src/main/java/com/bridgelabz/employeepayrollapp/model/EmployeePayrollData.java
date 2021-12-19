0738f3edb54e2e73543513afe97c81de71e5b112
shindeanant <shindeanant1997@gmail.com>
2021-12-19 17:18:16 +0530

    Added:Ability to save EmployeePayroll Data to MYSQL DB.

--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -2,27 +2,52 @@ package com.bridgelabz.employeepayrollapp.model;
 
 import java.time.LocalDate;
 import java.util.List;
-
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
-
 import lombok.Data;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "employeepayroll_db")
 @Data
 public class EmployeePayrollData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "employee_id")
     private int employeeId;
+    @Column(name = "name")
     private String name;
+    @Column(name = "salary")
     private long salary;
+    @Column(name = "gender")
     private String gender;
+    @Column(name = "start_date")
     private LocalDate startDate;
+    @Column(name = "note")
     private String note;
+    @Column(name = "profile_pic")
     private String profilePic;
+
+    @ElementCollection
+    @CollectionTable(name = "employee_department",
+            joinColumns = @JoinColumn(name ="id"))
+    @Column(name = "departments")
     private List<String> departments;
 
     public EmployeePayrollData() {
     }
+    public EmployeePayrollData(EmployeePayrollDTO employeePayrollDTO){
+        this.updateEmployeePayollData(employeePayrollDTO);
+    }
 
-    public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) {
-        this.employeeId = employeeId;
+    public void updateEmployeePayollData( EmployeePayrollDTO employeePayrollDTO) {
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
         this.gender = employeePayrollDTO.gender;
