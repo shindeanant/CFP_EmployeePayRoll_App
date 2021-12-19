cd21cde1c92792a7bdfb99741cdd9beba453f5cd
shindeanant <shindeanant1997@gmail.com>
2021-12-19 15:33:21 +0530

    Added;Ability to adding validation to Name filed.

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,13 +1,22 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeePayrollDTO {
+    @Pattern(regexp = "^[A-Z][a-zA-z]{2,}$", message = "Employee Name Invalid")
     public String name;
+    @Min(value = 10000, message = "Min Wage should be more than 10000")    
     public long salary;
 
-    public EmployeePayrollDTO(String name, long salary) {
-        this.name = name;
-        this.salary = salary;
-    }
+   
 
   
 }
