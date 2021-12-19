940117f17de1e04cf33e0d04d81a15dbae0bbbf7
shindeanant <shindeanant1997@gmail.com>
2021-12-19 15:15:29 +0530

    Added:Ability to set Environment Variable.

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -3,7 +3,11 @@ package com.bridgelabz.employeepayrollapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
