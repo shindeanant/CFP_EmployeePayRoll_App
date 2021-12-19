adad56668b7c4ed12e76b2daa8f0075e256e56ac
shindeanant <shindeanant1997@gmail.com>
2021-12-19 14:46:55 +0530

    Added: Ability to use Lombok library for Logging.

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -18,45 +18,83 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/employeePayrollservice")
+@Slf4j
 public class EmployeePayrollController {
 
-    
     @Autowired
     private IEmployeePayrollService employeePayrollService;
-    
-    @RequestMapping(value= {"","/","/get"})
-    public ResponseEntity<ResponseDTO> getEmployeePayrollData(){
+
+    @RequestMapping(value = { "", "/", "/get" })
+    public ResponseEntity<ResponseDTO> getEmployeePayrollData() {
         List<EmployeePayrollData> empDataList = null;
         empDataList = employeePayrollService.getEmployeePayrollData();
-        ResponseDTO respDTO = new ResponseDTO("Get Call Success",empDataList);
-        return new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
-    }
-    @GetMapping("/get/{empId}")
-    public ResponseEntity<ResponseDTO> getEmployeePayrollData(@PathVariable("empId") int empId){
-        EmployeePayrollData employeePayrollData = employeePayrollService.getEmployeePayrollDataById(empId);
-        ResponseDTO respDTO = new ResponseDTO("Get call for ID Successful:",employeePayrollData);
-        return new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK); 
-    }
-    @PostMapping("/create")
-    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO){
-        EmployeePayrollData employeePayrollData = employeePayrollService.createEmployeePayrollData(empPayrollDTO);
-        ResponseDTO respDTO = new ResponseDTO("Create Employee PayrollData:",employeePayrollData);
-        return new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
-    }
-    @PutMapping("/update/{empId}")
-    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId,@RequestBody EmployeePayrollDTO empPayrollDTO){
-       EmployeePayrollData empData= null;
-        empData = employeePayrollService.updateEmployeePayrollData(empId, empPayrollDTO);
-        ResponseDTO respDTO = new ResponseDTO("Update Employee PayrollData Successful:",empData);
-        return new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
+        ResponseDTO respDTO = new ResponseDTO("Get Call Success", empDataList);
+        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 
     }
+
+    /**
+     * Purpose : Ability to fetch employee details from Employee Payroll using ID
+     * 
+     * @param id
+     * @return
+     */
+    @GetMapping("/get/{empId}")
+    public ResponseEntity<ResponseDTO> getEmployeePayrollData(@PathVariable("empId") int empId) {
+        log.info("Inside getEmployeeDetails()");
+        EmployeePayrollData employeePayrollData = employeePayrollService.getEmployeePayrollDataById(empId);
+        ResponseDTO respDTO = new ResponseDTO("Get call for ID Successful:", employeePayrollData);
+        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+
+    }
+
+    /**
+     * Purpose : Ability to add employee details in Employee Payroll
+     * 
+     * @param employeeDTO
+     * @return
+     */
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO> addEmployeePayrollData(@RequestBody EmployeePayrollDTO empPayrollDTO) {
+        log.info("Inside addEmployeeDetails()");
+        EmployeePayrollData employeePayrollData = employeePayrollService.createEmployeePayrollData(empPayrollDTO);
+        ResponseDTO respDTO = new ResponseDTO("Create Employee PayrollData:", employeePayrollData);
+        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+    }
+
+    /**
+     * Purpose : Ability to update employee details in Employee Payroll using ID
+     * 
+     * @param id
+     * @param employeeDTO
+     * @return
+     */
+    @PutMapping("/update/{empId}")
+    public ResponseEntity<ResponseDTO> updateEmployeePayrollData(@PathVariable("empId") int empId,
+            @RequestBody EmployeePayrollDTO empPayrollDTO) {
+        log.info("Inside updateEmployeeDetails()");
+        EmployeePayrollData empData = null;
+        empData = employeePayrollService.updateEmployeePayrollData(empId, empPayrollDTO);
+        ResponseDTO respDTO = new ResponseDTO("Update Employee PayrollData Successful:", empData);
+        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
+
+    }
+
+    /**
+     * Purpose : Ability to delete employee details from Employee Payroll using ID
+     * 
+     * @param id
+     * @return
+     */
     @DeleteMapping("/delete/{empId}")
-    public ResponseEntity<ResponseDTO> deleteEmployeePayrollData(@PathVariable("empId") int empId){
+    public ResponseEntity<ResponseDTO> deleteEmployeePayrollData(@PathVariable("empId") int empId) {
         employeePayrollService.deleteEmployeePayrollData(empId);
-        ResponseDTO respDTO = new ResponseDTO("Deleted Successful,Deleted Id:",empId);
-        return new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
+        ResponseDTO respDTO = new ResponseDTO("Deleted Successful,Deleted Id:", empId);
+        return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
     }
 }
